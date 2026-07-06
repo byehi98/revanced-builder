343d760dc165646d4e2256041d13f7d1c8d2b968
byehi98 <byehi5321@gmail.com>
2026-07-06 11:22:34 +0530

    Improve APKMirror URL resolution in dl_apkmirror
    Attempt to resolve the release URL directly from the search response
    before falling back to the manual name construction logic.

--- utils.sh
+++ utils.sh
@@ -503,9 +503,16 @@ dl_apkmirror() {
 
 	if [ "$arch" = "arm-v7a" ]; then arch="armeabi-v7a"; fi
 	local resp node app_table apkmname dlurl=""
-	apkmname=$($HTMLQ "h1.marginZero" --text <<<"$__APKMIRROR_RESP__")
-	apkmname="${apkmname,,}" apkmname="${apkmname// /-}" apkmname="${apkmname//[^a-z0-9-]/}"
-	url="${url}/${apkmname}-${version//./-}-release/"
+	local release_url=""
+	release_url=$(grep -oP "href=\"(/apk/[^\"]*-${version//./-}-release/)\"" <<<"$__APKMIRROR_RESP__" | head -1 | cut -d'"' -f2)
+	if [ -n "$release_url" ]; then
+		url="https://www.apkmirror.com${release_url}"
+	else
+		apkmname=$($HTMLQ "h1.marginZero" --text <<<"$__APKMIRROR_RESP__")
+		apkmname="${apkmname,,}" apkmname="${apkmname//./-}" apkmname="${apkmname//+/-}" apkmname="${apkmname//_/-}" apkmname="${apkmname// /-}" apkmname="${apkmname//[^a-z0-9-]/}"
+		apkmname=$(echo "$apkmname" | tr -s '-')
+		url="${url}/${apkmname}-${version//./-}-release/"
+	fi
 	resp=$(req "$url" -) || return 1
 	node=$($HTMLQ "div.table-row.headerFont:nth-last-child(1)" -r "span:nth-child(n+3)" <<<"$resp")
 	if [ "$node" ]; then
